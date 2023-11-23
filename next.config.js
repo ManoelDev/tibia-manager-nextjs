3f8e5672ee807bdb8387e06528580c03e536b93d
ManoelDev <maguserv@gmail.com>
2023-11-23 00:26:14 -0300

    update status

--- next.config.js
+++ next.config.js
@@ -27,7 +27,29 @@ const nextConfig = {
 			allowedForwardedHosts: ['localhost', 'progamo.com.br'],
 			allowedOrigins: ['http://localhost', 'progamo.com.br', 'http://progamo.com.br', 'https://www.progamo.com.br']
 		},
-	}
+	},
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        // https://vercel.com/guides/how-to-enable-cors
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://dev.progamo.com.br" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
---
+++ src/app/api/status/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server"
+import configLua from '@/hooks/configLua'
+import { StatusServer } from '@/utils/statusServer'
+import { prisma } from "@/lib/prisma";
+
+const lua = configLua()
+
+async function getStatus() {
+  try {
+    const statusServer = new StatusServer();
+    const host = lua['ip']
+    const port = +lua['statusProtocolPort'];
+    const status = await statusServer.getStatus(host, port);
+    return {
+      status: !!status,
+    };
+  } catch (error) {
+    console.error('Ocorreu um erro ao verificar o status do servidor:', error);
+    return {
+      status: false,
+    };
+  }
+}
+
+
+const handle = async (request: NextRequest) => {
+  const status = await getStatus()
+  const playersOnline = await prisma.players_online.count()
+  return NextResponse.json({ status, playersOnline })
+}
+
+export { handle as GET }
