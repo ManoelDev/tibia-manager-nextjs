061a63cd9933ba67ede5ec4d5c72801f52babe99
ManoelDev <maguserv@gmail.com>
2023-12-04 23:18:16 -0300

    text in page recovery

--- src/app/account-manager/(auth)/recovery/page.tsx
+++ src/app/account-manager/(auth)/recovery/page.tsx
@@ -19,8 +19,23 @@ export default function Recovery() {
         <CardTitle>Recover by Email or Character Name</CardTitle>
       </CardHeader>
       <CardContent className="grid grid-cols-2 gap-2">
-        <div>
-          Texto aqui
+        <div className="text-xs space-y-2 border rounded p-2">
+          <div>
+            Welcome to the Lost Account Interface!
+            If you have lost access to your account, this interface can help you. Of course, you need to prove that your claim to the account is justified. Enter the requested data and follow the instructions carefully. Please understand there is no way to get access to your lost account if the interface cannot help you. Further options to change account data are available if you have a registered account.
+          </div>
+
+          <div>
+            By using the Lost Account Interface you can
+          </div>
+
+          <li>get a new password if you have lost the current password,</li>
+          <li>get your account back if it has been hacked,</li>
+          <li>change the email address of your account instantly (only possible with a valid recovery key or a valid recovery TAN),</li>
+          <li>request a new recovery key/recovery TAN (only available to registered accounts),</li>
+          <li>remove an authenticator app from your account (only possible with a valid recovery key or a valid recovery TAN),</li>
+          <li>disable email code authentication for your account (only available to accounts with a valid recovery key).</li>
+          <li>As a first step to use the Lost Account Interface, please enter the name of a character or the email address of your account and click on "Submit".</li>
         </div>
         <div><UserRecoveryForm /></div>
       </CardContent>
